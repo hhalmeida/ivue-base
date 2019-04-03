c1eae140d027dc6cfeed3719a251153d4baa597d
Hugo Almeida <hhalmeida3@gmail.com>
2019-04-03 16:43:02 -0300

    adding pushNotifications

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<ion-app>
+  <ion-app>
     {{#router}}
     <router-view/>
     {{else}}
@@ -17,11 +17,37 @@ export default {
   name: 'App'{{#router}}{{else}},
   components: {
     HelloWorld
-  }{{/router}}
+  },{{/router}}
+  data() {
+    return {
+      notificaions: [],
+      mobile: {}
+    }
+  },
+  created () {
+    PushNotifications.addListener('registrationError', (error) => { console.log(error) })
+    PushNotifications.addListener('registration', (token) => {
+      this.mobile.pnToken = token.value
+      Device.getInfo().then((data) => {
+        this.mobile.devUUID = data.uuid
+        this.mobile.platform = data.platform
+      }).then(() => {
+        window.localStorage.setItem('mobile', JSON.stringify(token.value))
+        // this.auth.saveUser(this.user)
+        PushNotifications.addListener('pushNotificationReceived', (notification) => { console.log(notification) })
+      })
+    })
+    PushNotifications.register()
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:300');
+  @import "./scss/_font.scss";
+  @import "./scss/_type.scss";
+  @import "./scss/_colors.scss";
+  @import "./scss/_font.scss";
   ion-page{ 
     flex: 1;
   }
